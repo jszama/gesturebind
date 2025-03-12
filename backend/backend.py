f47d99d7441d3c6d1975631c044344ef0e075913
jszama <jmszamotulski@gmail.com>
2025-03-12 20:59:22 +0000

    fixed backed not running

--- backend/backend.py
+++ backend/backend.py
@@ -8,17 +8,6 @@ from waitress import serve
 from GestureDetector import GestureDetector
 from PackageInstaller import PackageInstaller
 
-@app.route('/process-frame', methods=['POST'])
-def process_frame():
-    print("Processing frame")
-    data = request.json
-    img_data = base64.b64decode(data['image'].split(',')[1])
-    np_arr = np.frombuffer(img_data, np.uint8)
-    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-
-    gesture = gesture_detector.detect_gesture(img)
-    return jsonify({'gesture': gesture})
-
 if __name__ == '__main__':
     app = Flask(__name__)
 
@@ -26,5 +15,16 @@ if __name__ == '__main__':
     package_installer.install_missing_packages()
     
     gesture_detector = GestureDetector()
-        
+    
+    @app.route('/process-frame', methods=['POST'])
+    def process_frame():
+        print("Processing frame")
+        data = request.json
+        img_data = base64.b64decode(data['image'].split(',')[1])
+        np_arr = np.frombuffer(img_data, np.uint8)
+        img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        gesture = gesture_detector.detect_gesture(img)
+        return jsonify({'gesture': gesture})
+    
     serve(app, host='127.0.0.1', port=5000)
